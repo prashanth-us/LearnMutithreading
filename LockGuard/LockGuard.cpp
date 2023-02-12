7ae302aa794f0dbbc297dc51b88efc28dd9378b9
Prashanth Kumar U S <prashanthkumarus@gmail.com>
2023-02-12 19:51:41 +0530

    Lock Guard is added

---
+++ LockGuard/LockGuard.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <thread>
+#include <chrono>
+#include <mutex>
+
+void work(int &count, std::mutex &mtx)
+{
+    for (size_t i = 0; i < 1E6; i++)
+    {
+        std::lock_guard<std::mutex> guard(mtx);
+        ++count;
+    }
+}
+int main()
+{
+    int count = 0;
+    std::mutex mtx;
+
+    std::thread t1(work, std::ref(count), std::ref(mtx));
+    std::thread t2(work, std::ref(count), std::ref(mtx));
+
+    t1.join();
+    t2.join();
+
+    std::cout << "count: " << count << std::endl;
+    return 0;
+}
